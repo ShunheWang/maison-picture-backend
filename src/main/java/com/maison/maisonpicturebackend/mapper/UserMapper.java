da95164dd76cf47efd789687ba2a519feda197d7
ShunheWang <maisonwangsh@gmail.com>
2025-09-06 02:19:22 +0800

    feature: 项目初始化 init() ;

---
+++ src/main/java/com/maison/maisonpicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.maison.maisonpicturebackend.mapper;
+
+import com.maison.maisonpicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author shunhewang
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-09-06 01:38:02
+* @Entity com.maison.maisonpicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
